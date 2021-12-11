45ef732d38e73a3a2e5c4c8be9aab1f4ca5ec56a
Victor Andre de Moraes <victorandre957@gmail.com>
2021-12-11 12:54:38 -0300

    create nav and footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,19 @@
+import {Container} from './styles'
+
+
+const Footer = () => {
+    return (
+        <Container>
+            <footer>
+                <h4>Fale conosco</h4>
+                <ul>
+                    <li><h5> 0800 1234 6789</h5><h5>0800 6789 1234</h5></li>
+                    <li><p> Terça a Domingo: 18:00 a 00:00</p> </li>
+                </ul>
+            </footer>
+        </Container>
+    );
+}
+
+
+export default Footer;
---
+++ src/components/nav-bar/styles.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.nav `
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
+
+* {
+    margin: 0 auto;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Open Sans', sans-serif;
+}
+
+#menu-h{
+    background-color: #FF5C01;
+}
+
+#menu-h ul {
+    max-width: 100%;
+    list-style: none;
+    padding: -5rem 0rem;
+}
+
+#menu-h ul li {
+    display: inline;
+}
+
+#menu-h ul li a {
+    color: #FFF;
+    padding: 1.25rem;
+    display: inline-block;
+    text-decoration: none;
+    margin: -0.5rem 8rem;
+    font-weight: bold;
+}
+
+#menu-h ul li:last-child a {
+    float: right;
+    margin-right: 0rem;
+}
+
+`;
