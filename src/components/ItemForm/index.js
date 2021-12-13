f35347d3be204afed602bda4e8c5db63c5cec752
mathuscada <matheusbtourinho@gmail.com>
2021-12-12 21:04:29 -0300

    ✨ Update Product/Category and Delete Category

--- src/components/ItemForm/index.js
+++ src/components/ItemForm/index.js
@@ -4,27 +4,30 @@ import { ButtonDiv, CategoriesDiv, Container, Form, ProductDiv } from './styles'
 import ProductCard from './ProductCard'
 import CategoryCard from './CategoryCard'
 import ItemDefault from '../../assets/item_default.png'
+import { useNavigate } from 'react-router'
 
 const ProductForm = () => {
     const [products, setProducts] = useState([])
+
     const [categories, setCategories] = useState([])
-    const placeholderProduct = {
-        id: 0,
-        name: "Novo produto",
-        price: 0,
-        description: "Coloque uma descrição para o produto"
-    }
+
     const [selectedType, setSelectedType] = useState("products")
-    const [selectedItem, setSelectedItem] = useState(placeholderProduct)
+
+    const [selectedItem, setSelectedItem] = useState({})
+
+    const navigate = useNavigate()
+
     const getProducts = async () => {
         api_v1.get('products').then((response) => {
-            setProducts([...response.data, placeholderProduct])
+            setProducts([...response.data])
+            setSelectedItem({...response.data[0], photo: response.data[0].photo_url})
         })
     }
 
     const getCategories = async () => {
         api_v1.get('types').then((response) => {
             setCategories(response.data)
+            setSelectedItem(response.data[0])
         })
     }
 
@@ -35,6 +38,89 @@ const ProductForm = () => {
         else
             getCategories();
     }, [selectedType])
+
+    const editProduct = async (e) => {
+        e.preventDefault()
+        const response = validateProduct()
+        if(response === "ok") {
+            api_v1.defaults.headers.common[`X-Admin-Token`] = "dJ8zLtntmHrh_h4cgPD4"
+            api_v1.defaults.headers.common[`X-Admin-Email`] = "boss@final"
+            const api_response = await api_v1.patch(`products/update/${selectedItem.id}`,{
+                product: {
+                    name: selectedItem.name,
+                    price: selectedItem.price,
+                    description: selectedItem.description
+                }
+            }).then(response => {
+                alert("Produto alterado com sucesso")
+                window.location.reload()
+            }).catch((e) =>
+                console.log(e)
+            )
+        }
+        else {
+            alert(response)
+        }
+    }
+
+    const validateProduct = () => {
+        if(!selectedItem.name) {
+            return "Coloque um nome válido"
+        }
+        else if(!selectedItem.price) {
+            return "Coloque um preço válido"
+        }
+        else if(!selectedItem.description) {
+            return "Coloque uma descrição válida"
+        }
+        else {
+            return "ok"
+        }
+    }
+
+    const editCategory = async (e) => {
+        e.preventDefault()
+        const response = validateCategory()
+        if(e.nativeEvent.submitter.name === "edit_button") {
+            if(response === "ok") {
+                api_v1.defaults.headers.common[`X-Admin-Token`] = "dJ8zLtntmHrh_h4cgPD4"
+                api_v1.defaults.headers.common[`X-Admin-Email`] = "boss@final"
+                const api_response = await api_v1.patch(`types/update/${selectedItem.id}`,{
+                    type: {
+                        name: selectedItem.name
+                    }
+                }).then(response => {
+                    alert("Categoria alterada com sucesso")
+                    window.location.reload()
+                }).catch((e) =>
+                    console.log(e)
+                )
+            }
+            else {
+                alert(response)
+            }
+        }
+        else {
+            api_v1.defaults.headers.common[`X-Admin-Token`] = "dJ8zLtntmHrh_h4cgPD4"
+            api_v1.defaults.headers.common[`X-Admin-Email`] = "boss@final"
+            const response = await api_v1.delete(`types/delete/${selectedItem.id}`
+            ).then(response => {
+                alert("Categoria deletada com sucesso")
+                window.location.reload()
+            }).catch((e) =>
+                console.log(e)
+            )
+        }
+    }
+
+    const validateCategory = () => {
+        if(!selectedItem.name) {
+            return "Coloque um nome válido"
+        }
+        else {
+            return "ok"
+        }
+    }
 
     return (
         <Container>
@@ -59,18 +145,19 @@ const ProductForm = () => {
             }
             {
                 selectedType==="products"?
-                <Form>
+                <Form onSubmit={editProduct}>
                     <img src={selectedItem.photo? controller.defaults.baseURL+selectedItem.photo : ItemDefault} alt="foto do produto"/>
                     <input type="file"/>
-                    <input placeholder="Nome" type="text" value={selectedItem.name}/>
-                    <input placeholder="Preço" type="text" value={selectedItem.price}/>
-                    <textarea placeholder="Descrição" type="text" value={selectedItem.description}/>
-                    <button>Adicionar/Alterar Produto</button>
-                    <button>Remover Produto</button>
+                    <input onChange={(e) => setSelectedItem({...selectedItem, name: e.target.value})} placeholder="Nome" type="text" value={selectedItem.name}/>
+                    <input onChange={(e) => setSelectedItem({...selectedItem, price: e.target.value})} placeholder="Preço" type="text" value={selectedItem.price}/>
+                    <textarea onChange={(e) => setSelectedItem({...selectedItem, description: e.target.value})} placeholder="Descrição" type="text" value={selectedItem.description}/>
+                    <button type='submit'>Alterar Produto</button>
                 </Form>
                 :
-                <Form>
-
+                <Form onSubmit={editCategory}>
+                    <input onChange={(e) => setSelectedItem({...selectedItem, name: e.target.value})} value={selectedItem.name} placeholder="Nome" type="text"/>
+                    <button type='submit' name='edit_button'>Alterar Categoria</button>
+                    <button type='submit' name='delete_button'>Deletar Categoria</button>
                 </Form>
             }
         </Container>
